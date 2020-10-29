6f956ab94562e7bbe5edad2f6c94da3af1a6603a
Artem <artemtkachuk@yahoo.com>
2020-10-29 07:19:42 +0300

    another experiment

--- helper.py
+++ helper.py
@@ -1,13 +1,26 @@
 from math import factorial
 
 
-def compare(a, b, operation):
-    assert operation == '==' or operation == '>=', 'operation must be == or >=!'
+def compare(outcome, b, value, operation, n):
+    """
+        :param outcome: one outcome of the experiment we are performing
+        :param b: used only for '==' and '>=' where we look at single bin. Denotes the bin that we are examining.
+        :param value: Denotes the # of balls that we want to check the bin(s) for
+        :param operation: determines the way we compare the values we provided
+        :param n: number of bins. Needed only for 'count' since there we check every bin
+        :return: returns whether a condition is true
+    """
+    assert operation == '==' or operation == '>=' or operation == 'count', 'operation must be == or >=! or count!'
 
     if operation == '==':
-        return a == b
-    else:
-        return a >= b
+        return outcome.count(b) == value
+    elif operation == '>=':
+        return outcome.count(b) >= value
+    elif operation == 'count':
+        for i in range(1, n + 1):
+            if outcome.count(i) == value:
+                return True
+        return False
 
 
 def choose(n, k):
--- main.py
+++ main.py
@@ -4,16 +4,18 @@ from helper import compare, choose
 
 def run_simulation(n, m, j, k, operation, num_trials):
     """
-        N - bins
-        M - balls
-        J - which bin we are interested in
-        K - how many balls fall into J's bin
+        :param n - # bins
+        :param m - # balls
+        :param j - which bin we are interested in
+        :param k - how many balls fall into J's bin
+        :param operation - what we want to kno about the trials set
+        :param num_trials - # times we run experiment
     """
     success_trials = 0
 
     for i in range(num_trials):
         outcome = [randint(1, n) for _ in range(m)]
-        if compare(outcome.count(j), k, operation):
+        if compare(outcome, j, k, operation, n):
             success_trials += 1
 
     return success_trials / num_trials
@@ -21,9 +23,9 @@ def run_simulation(n, m, j, k, operation, num_trials):
 
 def compute_binomial(n, m, k):
     """
-            N - bins
-            M - balls
-            K - bin has at most this # of balls
+        :param n - bins
+        :param m - balls
+        :param k - bin has at most this # of balls
     """
     return sum([choose(m, i) * (1/n)**i * ((n - 1) / n)**(m - i) for i in range(k + 1)])
 
@@ -33,13 +35,16 @@ def simulate_experiment():
     m = 7  # balls
     num_trials = 10000000
 
-    p_1_bin_has_exactly_3_balls = run_simulation(n, m, 1, 3, '==', num_trials)
+    estimated_p_1_bin_has_exactly_3_balls = run_simulation(n, m, 1, 3, '==', num_trials)
+    print(f'Estimated P[1st bin has exactly 3 balls] = {estimated_p_1_bin_has_exactly_3_balls}')
+
     estimated_p_3_bin_has_at_least_3_balls = run_simulation(n, m, 3, 3, '>=', num_trials)
     exact_p_3_bin_has_at_least_3_balls = 1 - compute_binomial(n, m, 2)
-
-    print(f'estimated P[1st bin has exactly 3 balls] = {p_1_bin_has_exactly_3_balls}')
-    print(f'estimated P[3rd bin has at least 3 balls] = {estimated_p_3_bin_has_at_least_3_balls}')
+    print(f'Estimated P[3rd bin has at least 3 balls] = {estimated_p_3_bin_has_at_least_3_balls}')
     print(f'Exact P[3rd bin has at least 3 balls] = {exact_p_3_bin_has_at_least_3_balls} ')
+
+    estimated_p_at_least_one_bin_with_exactly_3_balls = run_simulation(n, m, 0, 3, 'count', num_trials)
+    print(f'Estimated P[at least one bin has three balls] = {estimated_p_at_least_one_bin_with_exactly_3_balls}')
 
 
 simulate_experiment()
